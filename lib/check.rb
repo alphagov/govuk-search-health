4cff4c72fea5ac1cd815a209f8afc4f345ba563d
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-04-26 17:48:58 +0100

    Extract Calculator and scoring code

--- lib/check.rb
+++ lib/check.rb
@@ -1,4 +1,5 @@
 Check = Struct.new(:search_term, :imperative, :path, :minimum_rank, :weight)
+Result = Struct.new(:success, :score, :possible_score)
 
 class Check
   def valid_imperative?
@@ -23,5 +24,25 @@ class Check
 
   def positive_check?
     imperative == "should"
+  end
+
+  def result(search_results)
+    found_index = search_results.index { |url|
+      URI.parse(url).path == path
+    }
+
+    found_in_limit = found_index && found_index < minimum_rank
+    success = !!(positive_check? ? found_in_limit : ! found_in_limit)
+
+    marker = "[#{weight}-POINT #{success ? "SUCCESS" : "FAILURE"}]"
+    if found_index
+      expectation = positive_check? ? "<= #{minimum_rank}" : "> #{minimum_rank}"
+      puts "#{marker} Found '#{path}' for '#{search_term}' in position #{found_index + 1} (expected #{expectation})"
+    else
+      puts "#{marker} Didn't find '#{path}' in results for '#{search_term}'"
+    end
+
+    score = success ? weight : 0
+    Result.new(success, score, weight)
   end
 end
--- lib/check_search.rb
+++ lib/check_search.rb
@@ -3,6 +3,7 @@ require "uri"
 
 require_relative "check_file_parser"
 require_relative "search_client"
+require_relative "calculator"
 
 class CheckSearch
   def initialize(authentication, search_host, filename, api_format, slow)
@@ -11,41 +12,26 @@ class CheckSearch
   end
 
   def call
-    checks = CheckFileParser.new(File.open(@filename)).checks
-
-    success_count = total_count = score = total_score = 0
-
-    search_client = SearchClient.new(base_url: @base_url, authentication: @authentication, api_format: @api_format)
     checks.each do |check|
-      results = search_client.search(check.search_term)
-
-      found_index = results.index { |url|
-        URI.parse(url).path == check.path
-      }
-
-      total_count += 1
-      total_score += check.weight
-
-      found_in_limit = found_index && found_index < check.weight
-      success = check.positive_check? ? found_in_limit : ! found_in_limit
-
-      marker = "[#{check.weight}-POINT #{success ? "SUCCESS" : "FAILURE"}]"
-
-      success_count += 1 if success
-      score += check.weight if success
-
-      if found_index
-        expectation = check.positive_check? ? "<= #{check.minimum_rank}" : "> #{check.minimum_rank}"
-        puts "#{marker} Found '#{check.path}' for '#{check.search_term}' in position #{found_index + 1} (expected #{expectation})"
-      else
-        puts "#{marker} Didn't find '#{check.path}' in results for '#{check.search_term}'"
-      end
+      search_results = search_client.search(check.search_term)
+      result = check.result(search_results)
+      calculator.add(result)
 
       sleep 0.25 if @slow
     end
-
-    score_percentage = score.to_f / total_score * 100
-    puts "Score: #{score}/#{total_score} (#{format('%.2f', score_percentage)}%)"
-    puts "#{success_count} of #{total_count} succeeded"
+    calculator.summarise
   end
+
+  private
+    def checks
+      CheckFileParser.new(File.open(@filename)).checks
+    end
+
+    def search_client
+      @_search_client ||= SearchClient.new(base_url: @base_url, authentication: @authentication, api_format: @api_format)
+    end
+
+    def calculator
+      @_calculator ||= Calculator.new
+    end
 end
